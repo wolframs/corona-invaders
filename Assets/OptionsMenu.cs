bfa882365b65ff83bc5aa70fbb264e757f76a2f2
Wolfram S <wolframs91@gmail.com>
2020-04-27 14:29:18 +0200

    Sound Volume Control done

---
+++ Assets/OptionsMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer MusicMixer;
+    public AudioMixer SFXMixer;
+
+    public void SetMusicVolume(float volume)
+    {
+        MusicMixer.SetFloat("volume", volume);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        SFXMixer.SetFloat("volume", volume);
+    }
+}
